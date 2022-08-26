45991fc3770394b4d95ceb7286ed18e0b6184595
JayBal <jackybaloyi.jb@gmail.com>
2022-08-26 05:26:43 -0700

    Import error.

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3.4
-if __name__ == "__main__":
-    from hidden_4 import *
-    for s in dir():
-        if s[:2] != "__":
-            print("{}".format(s))
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0:2] != '__':
+            print(name)   
